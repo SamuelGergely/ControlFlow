ba94cd6719f0b2ce13189dcfee825f51a7818c20
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2024-09-26 14:16:44 +0200

    Feat: remove break function

--- src/weeklyDataSwitchCase.js
+++ src/weeklyDataSwitchCase.js
@@ -2,28 +2,21 @@ function getDayIfSwitchCase(jour_semaine) {
     switch (jour_semaine){
         case 1:
             return "Lundi"
-            break;
         case 2:
             return "Mardi"
-            break;
         case 3:
             return "Mercredi"
-            break;
         case 4:
             return "Jeudi"
-            break;
         case 5:
             return "Vendredi"
-            break;
         case 6:
             return "Samedi"
-            break;
         case 7:
             return "Dimanche"
-            break;
         default:
             return "Numéro de jour invalide"
     }
 }
 
-console.log(getDayIfSwitchCase(7));
+console.log(getDayIfSwitchCase(5));
